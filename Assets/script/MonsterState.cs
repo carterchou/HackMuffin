7efc3e038b624c9d9806b8dc29802e53c252d0be
xiang221 <s276581@gmail.com>
2021-12-19 10:59:30 +0800

    2D AI

--- Assets/script/MonsterState.cs
+++ Assets/script/MonsterState.cs
@@ -6,16 +6,25 @@ using UnityEngine.AI;
 
 public class MonsterState : MonoBehaviour
 {
-    
+
+    private NavMeshAgent Agent;
     public GameObject player;             
-    private bool nowstate = true;
     public float walkSpeed = 0.5f;
     private float diatanceToPlayer;
-    NavMeshAgent NMA;
+    //NavMeshAgent NMA;
+    Vector3 notarget;
+    Transform target;
+
 
     void Start(){
-        NMA = GetComponent<NavMeshAgent>();
-        player = monsterManager.GetInstance().player;
+        //NMA = GetComponent<NavMeshAgent>();
+        //player = monsterManager.GetInstance().player;
+
+        notarget = transform.position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
+        target = monsterManager.GetInstance().player.transform;
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.updateRotation = false;
+        Agent.updateUpAxis = false;
 
     }
 
@@ -23,15 +32,18 @@ public class MonsterState : MonoBehaviour
 
     void Action(){
         diatanceToPlayer = Vector2.Distance(player.transform.position, player.transform.position);
-        nowstate = true;
+        //nowstate = true;
         if (diatanceToPlayer <= 30)
         {
-            NMA.destination = player.transform.position;
+            //NMA.destination = player.transform.position;
+            Agent.SetDestination(target.position);
         }
         else
         {
-            Vector2 nextpos = new Vector2(transform.position.x, transform.position.y);
-            NMA.destination = nextpos;
+            //Vector2 nextpos = new Vector2(transform.position.x, transform.position.y);
+            //NMA.destination = nextpos;
+            //notarget = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
+            Agent.SetDestination(notarget); 
         }
     }
 
