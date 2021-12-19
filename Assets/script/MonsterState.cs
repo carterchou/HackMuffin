c4bf6abde9eb65d1adffe4753eb5910e2fa8bce1
carterchou <carter870808@gmail.com>
2021-12-19 12:40:38 +0800

    AI

--- Assets/script/MonsterState.cs
+++ Assets/script/MonsterState.cs
@@ -7,44 +7,71 @@ using UnityEngine.AI;
 public class MonsterState : MonoBehaviour
 {
 
-    private NavMeshAgent Agent;
-    public GameObject player;             
+    private NavMeshAgent Agent;           
     public float walkSpeed = 0.5f;
     private float diatanceToPlayer;
-    //NavMeshAgent NMA;
+
+
+    enum moveType
+    {
+        tooClose,
+        chase,
+        walk
+    }
+
+    moveType type = moveType.walk;
+
     Vector3 notarget;
     Transform target;
 
 
     void Start(){
         //NMA = GetComponent<NavMeshAgent>();
-        //player = monsterManager.GetInstance().player;
-
-        notarget = transform.position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
-        target = monsterManager.GetInstance().player.transform;
+       
+       
         Agent = GetComponent<NavMeshAgent>();
         Agent.updateRotation = false;
         Agent.updateUpAxis = false;
 
     }
 
-    //走路，若靠近玩家則會追趕
-
     void Action(){
-        diatanceToPlayer = Vector2.Distance(player.transform.position, player.transform.position);
-        //nowstate = true;
-        if (diatanceToPlayer <= 30)
-        {
-            //NMA.destination = player.transform.position;
-            Agent.SetDestination(target.position);
+        
+
+        if (GameManager.GetInstance().GetRoot()) {
+            target = GameManager.GetInstance().GetRoot().transform;
+            float distance = Vector2.Distance(transform.position, target.position);
+            if (distance <= 0.5f)
+            {
+                type = moveType.tooClose;
+            } else if (distance <= 10) {
+                type = moveType.chase;
+            }
+            else
+            {
+                notarget = transform.position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
+                type = moveType.walk;
+            }
+
         }
         else
         {
-            //Vector2 nextpos = new Vector2(transform.position.x, transform.position.y);
-            //NMA.destination = nextpos;
-            //notarget = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-            Agent.SetDestination(notarget); 
+            notarget = transform.position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
+            type = moveType.walk;
         }
+
+        switch (type) {
+            case moveType.tooClose:
+                Agent.SetDestination(transform.position);
+                break;
+            case moveType.chase:
+                Agent.SetDestination(target.position);
+                break;
+            case moveType.walk:
+                Agent.SetDestination(notarget);
+                break;
+        }
+
     }
 
 
